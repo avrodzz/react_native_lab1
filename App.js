eb9192b7cd490d7177f63a263fea64c24a9be9f4
Alexis Rodriguez <avrodzz@gmail.com>
2022-07-07 12:34:15 -0700

    Created a ScrollView and started working on imageContainer and nameTextContainer

--- App.js
+++ App.js
@@ -1,52 +1,93 @@
+import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, SafeAreaView, ScrollView, View, StyleSheet, Image} from "react-native";
 
 export default function App() {
   return (
+    // <SafeAreaView style={styles.container}>
+    //   <ScrollView>
+    //     <View style={styles.photoContainer}>
+    //     <Image source={require("./assets/perry.png")} style={styles.photo} />
+    //   </View>
+    //   <View style={styles.nameContainer}>
+    //     <Text style={styles.nameAndPronouns}>Alexis Rodriguez</Text>
+    //   </View>
+    //   <View style={styles.blurbContainer}>
+    //     <Text style={styles.blurb}>chirp chirp i'm a parakeet</Text>
+    //   </View>
+    //   </ScrollView>
+      
+    // </SafeAreaView>
+
+
     <View style={styles.container}>
-      <View style={styles.photoContainer}>
-        <Image source={require("./assets/perry.png")} style={styles.photo} />
-      </View>
-      <View style={styles.nameContainer}>
-        <Text style={styles.nameAndPronouns}>Perry (she/her)</Text>
-      </View>
-      <View style={styles.blurbContainer}>
-        <Text style={styles.blurb}>chirp chirp i'm a parakeet</Text>
-      </View>
+      <ScrollView style={styles.scrollViewContainer} contentContainerStyle={{flex: 1, flexDirection: 'column', justifyContent: 'space-evenly'}}>
+
+        {/* Image */}
+        <View style={styles.imageContainer}>
+          <Image source={require("./assets/perry.png")} style={styles.image} />
+        </View>
+
+        <View style={styles.nameTextContainer}>
+          <Text style={styles.nameText}>
+            Alexis Rodriguez
+          </Text>
+        </View>
+       
+
+{/* <View style={styles.photoContainer}>
+     <Image source={require("./assets/perry.png")} style={styles.photo} />
+    //   </View> */}
+
+
+      </ScrollView>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
   },
-  photoContainer: {
+  scrollViewContainer: {
+    backgroundColor: '#608cc1',
+  },
+  imageContainer: {
     flex: 3,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "yellow",
-    padding: 30,
+    padding: 10,
+
   },
-  nameContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  blurbContainer: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  photo: {
+  image: {
     resizeMode: "contain",
     width: "90%",
     padding: 50,
+    borderRadius: 100
   },
-  nameAndPronouns: {
+  text: {
+    fontSize: 42,
+  },
+  nameTextContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 0
+  },
+  nameText: {
     fontSize: 50,
   },
-  blurb: {
-    fontSize: 20,
-  },
+  // blurbContainer: {
+  //   flex: 2,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
+
+
+  // blurb: {
+  //   fontSize: 20,
+  // },
 });
